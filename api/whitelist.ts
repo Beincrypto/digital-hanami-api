27d3808f996870b043ef8fc8943c51932ed5328a
Jose F. Fernandez <jffernandez@fenix-es.com>
2022-03-29 15:42:03 +0200

    feat: Add base API

---
+++ api/whitelist.ts
@@ -0,0 +1,47 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import { MerkleTree } from 'merkletreejs'
+import { utils } from 'ethers'
+import mainLeaf from '../src/config/whitelist/mainnet.json'
+import testLeaf from '../src/config/whitelist/testnet.json'
+
+export const whitelist = async (
+  chainId: string,
+  address: string,
+): Promise<{
+  status: number
+  whitelisted?: boolean
+  proof?: string[]
+}> => {
+  let proof: string[] = []
+  let merkleTree
+  if (chainId === '1') {
+    // mainnet
+    merkleTree = new MerkleTree(mainLeaf, utils.keccak256, { sortPairs: true })
+  } else {
+    // testnet
+    merkleTree = new MerkleTree(testLeaf, utils.keccak256, { sortPairs: true })
+  }
+  const claimingAddressHash = utils.keccak256(address)
+  proof = merkleTree.getHexProof(claimingAddressHash)
+
+  return {
+    status: 200,
+    whitelisted: proof.length > 0,
+    proof,
+  }
+}
+
+export default async (req: VercelRequest, res: VercelResponse): Promise<void | VercelResponse> => {
+  // Read parameters
+  const chainId = req.query['chainId'] as string
+  const address = req.query['address'] as string
+  // Check all parameters OK
+  if (chainId === undefined || address === undefined) {
+    // Bad request
+    return res.status(400).end()
+  }
+  // Make process and get return data
+  const data = await whitelist(chainId, address)
+  // Send data and end connection
+  res.status(data.status).send(data).end()
+}
